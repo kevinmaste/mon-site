ee0e87524ed61307847dc9421749e6277a785729
kevinmaste <kevin.bitoumboubiboka@supinfo.com>
2022-07-19 03:19:56 +0200

    Updates

---
+++ precache-manifest.88ea0d70d4e346921d8445d9be6e1c85.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c3e51512c8516e375c3383c9d606b75",
+    "url": "/index.html"
+  },
+  {
+    "revision": "934d5e38fc3f2ccf5b57",
+    "url": "/static/css/main.668d3648.chunk.css"
+  },
+  {
+    "revision": "f9e6685810cf3cf557bb",
+    "url": "/static/js/2.8a9b2612.chunk.js"
+  },
+  {
+    "revision": "e9256ddc4faa5439b55717202e329355",
+    "url": "/static/js/2.8a9b2612.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "934d5e38fc3f2ccf5b57",
+    "url": "/static/js/main.708be794.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "36d1a70d7635097c0ab6111b584b0253",
+    "url": "/static/media/commerce.36d1a70d.png"
+  },
+  {
+    "revision": "59edbe0a7191cd2a97b42533d8235886",
+    "url": "/static/media/kevin.59edbe0a.png"
+  },
+  {
+    "revision": "c9e1b7fbe23d57e1097570abdb0083c5",
+    "url": "/static/media/starbucksclone.c9e1b7fb.png"
+  },
+  {
+    "revision": "e15f665d4f7919f99f7330cfdeb5448f",
+    "url": "/static/media/webmaniac.e15f665d.png"
+  },
+  {
+    "revision": "e5a93568f2986d1ab6279a1bb01c30f2",
+    "url": "/static/media/zinformatique.e5a93568.png"
+  }
+]);
